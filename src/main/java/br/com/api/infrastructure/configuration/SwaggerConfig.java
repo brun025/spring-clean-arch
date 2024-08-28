7eb455a04ea5a415079ef6f863460dd19ad36594
brun025 <brunofelipedk@gmail.com>
2024-08-28 14:05:20 -0300

    Adicionando documentação com swagger

---
+++ src/main/java/br/com/api/infrastructure/configuration/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package br.com.api.infrastructure.configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder()
+				.group("public-apis")
+				.pathsToMatch("/**")
+				.build();
+	}
+	
+	@Bean
+	OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.info(new Info().title("API Clean Arch").version("1.0"))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(
+						new Components()
+						.addSecuritySchemes("bearerAuth", new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")));		
+	}
+}
