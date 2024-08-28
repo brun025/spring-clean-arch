a44e6818d00ec729c9823227f403b57e372e23d5
brun025 <brunofelipedk@gmail.com>
2024-08-28 13:51:11 -0300

    Implementando documentação com Swagger

--- src/main/java/br/com/api/infrastructure/controllers/UserController.java
+++ src/main/java/br/com/api/infrastructure/controllers/UserController.java
@@ -1,6 +1,5 @@
 package br.com.api.infrastructure.controllers;
 
-
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
@@ -16,11 +15,16 @@ import br.com.api.infrastructure.dtos.CreateUserDto;
 import br.com.api.infrastructure.dtos.UpdateUserDto;
 
 import org.springframework.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Tag(name = "UserEndpoint") 
 @RestController
 @RequestMapping("api/v1/users")
 @AllArgsConstructor
@@ -33,6 +37,13 @@ public class UserController {
     private final DeleteUserUseCase deleteUserUseCase;
     private final UserDtoMapper pessoaDtoMapper;
 
+    @Operation(summary = "Realiza o cadastro de usuários", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cadastro de usuário realizado com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar o cadastro de usuário"),
+    })
     @PostMapping(produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public User createPessoa(@RequestBody @Valid CreateUserDto userDTO) {
@@ -40,6 +51,13 @@ public class UserController {
         return newUser;
     }
 
+    @Operation(summary = "Listagem de todos os usuários", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
+    })
     @GetMapping(produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public List<User> obtainAll() {
@@ -48,6 +66,14 @@ public class UserController {
                 .collect(Collectors.toList());
     }
     
+    @Operation(summary = "Busca um usuário", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
+    })
     @GetMapping(produces = "application/json", value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Optional<User> getById(@PathVariable Long id) {
@@ -55,6 +81,14 @@ public class UserController {
         return user;
     }
     
+    @Operation(summary = "Atualiza um usuário", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Atualização de usuário realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a atualização de usuário"),
+    })
     @PutMapping(produces = "application/json", value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public User updateUser(@PathVariable Long id, @RequestBody @Valid UpdateUserDto userDTO) {
@@ -62,6 +96,14 @@ public class UserController {
         return user;
     }
     
+    @Operation(summary = "Remove um usuário", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "2004", description = "Remoção realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados"),
+    })
     @DeleteMapping(produces = "application/json", value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Long id) {
