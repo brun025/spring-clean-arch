2d22d75562af78cb2d32e15f1cddfe42e5109363
brun025 <brunofelipedk@gmail.com>
2024-09-21 18:07:05 -0300

    Refactor

--- src/main/java/br/com/api/infrastructure/advice/RestErrorMessage.java
+++ src/main/java/br/com/api/infrastructure/advice/RestErrorMessage.java
@@ -2,15 +2,30 @@ package br.com.api.infrastructure.advice;
 
 import org.springframework.http.HttpStatus;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@Getter
-@Setter
 public class RestErrorMessage {
 
 	private HttpStatus status;
 	private String message;
+	
+	public RestErrorMessage(HttpStatus status, String message) {
+		super();
+		this.status = status;
+		this.message = message;
+	}
+	
+	public HttpStatus getStatus() {
+		return status;
+	}
+	
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
